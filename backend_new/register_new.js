1fb5877b6c72c7e2b0e4e823857b67500b505e6d
Stefan Anders <stefananders@hotmail.de>
2023-08-08 22:23:55 +0200

    added documentation

--- backend_new/register_new.js
+++ backend_new/register_new.js
@@ -5,6 +5,9 @@ async function init(){
     loadUsers();
 }
 
+/**
+ * This function loads all the users in the database
+ */
 async function loadUsers(){
     let registeredUsers = document.getElementById('registeredUsers');
 
@@ -17,7 +20,9 @@ async function loadUsers(){
     registeredUsers.innerHTML = 'Usermail: ' + users[0]['email'];
 }
 
-
+/**
+ * This function adds a new user to the database
+ */
 async function register() {
     registerBtn.disabled = true;
     users.push({
@@ -28,6 +33,9 @@ async function register() {
     resetForm();
 }
 
+/**
+ * This function clears the input fields from the register form
+ */
 function resetForm() {
     email.value = '';
     password.value = '';
--- backend_new/storage_new.js
+++ backend_new/storage_new.js
@@ -1,6 +1,13 @@
 const STORAGE_TOKEN = "RXLCNTQEEUKVMM15Z3NQDLENEB6XNQRXMGLA5TH9";
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
+/**
+ * This function is used to transfer data(value) with a specified key to the server 
+ * 
+ * @param {string} key 
+ * @param {string} value 
+ * @returns {JSON}
+ */
 async function setItem(key, value) {
   const payload = { key, value, token: STORAGE_TOKEN };
   return fetch(STORAGE_URL, {
@@ -9,6 +16,12 @@ async function setItem(key, value) {
   }).then((res) => res.json());
 }
 
+/**
+ * This function gets the data (specified by a key) from the server
+ * 
+ * @param {string} key 
+ * @returns {JSON}
+ */
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
   return fetch(url)
